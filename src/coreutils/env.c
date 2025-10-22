4cd55c4711005a2278386911f07e95a7d26dbae9
twbaty <51901395+twbaty@users.noreply.github.com>
2025-10-22 15:37:14 -0500

    Update env.c

--- src/coreutils/env.c
+++ src/coreutils/env.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    extern char **environ;
-    for(char **p=environ;*p;++p) printf("%s\n",*p);
+int main(void) {
+#ifdef _WIN32
+    extern char **_environ;   // Windows-specific name
+    char **environ = _environ;
+#else
+    extern char **environ;    // POSIX
+#endif
+
+    for (char **p = environ; *p; ++p)
+        printf("%s\n", *p);
+
     return 0;
 }
