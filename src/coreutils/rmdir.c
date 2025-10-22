ef4adfa96320d1e80e7ead7a615136004cc15daf
twbaty <51901395+twbaty@users.noreply.github.com>
2025-10-22 15:00:14 -0500

    Use platform-specific rmdir function

--- src/coreutils/rmdir.c
+++ src/coreutils/rmdir.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
+#ifdef _WIN32
+#include <direct.h>  // for _rmdir
+#else
+#include <unistd.h>  // for rmdir
+#endif
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: rmdir <directory>\n");
         return 1;
     }
 
-    if (rmdir(argv[1]) != 0)
+#ifdef _WIN32
+    int result = _rmdir(argv[1]);
+#else
+    int result = rmdir(argv[1]);
+#endif
+
+    if (result != 0)
         perror(argv[1]);
 
     return 0;
