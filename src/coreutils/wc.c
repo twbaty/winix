dc63748341e3410eeb63c6cdd584b5b1d4e81b4f
twbaty <51901395+twbaty@users.noreply.github.com>
2025-10-23 10:54:06 -0500

    Update wc.c

--- src/coreutils/wc.c
+++ src/coreutils/wc.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) { fprintf(stderr,"Usage: wc <file>\n"); return 1; }
-    FILE *f=fopen(argv[1],"r"); if(!f){perror("wc");return 1;}
-    int c; long lines=0, words=0, bytes=0; int inword=0;
-    while((c=fgetc(f))!=EOF){bytes++; if(c=='\n')lines++;
-        if((c==' '||c=='\n'||c=='\t')) inword=0;
-        else if(!inword){inword=1; words++;}}
-    fclose(f);
-    printf("%ld %ld %ld %s\n", lines, words, bytes, argv[1]);
+int main(int argc, char* argv[]) {
+    int lines = 0, words = 0, chars = 0;
+    int total_lines = 0, total_words = 0, total_chars = 0;
+
+    if (argc == 1) {
+        // No args → read stdin
+        int c, in_word = 0;
+        while ((c = fgetc(stdin)) != EOF) {
+            chars++;
+            if (c == '\n') lines++;
+            if (isspace(c)) in_word = 0;
+            else if (!in_word) { words++; in_word = 1; }
+        }
+        printf("%10d %10d %10d\n", lines, words, chars);
+        return 0;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        FILE* f = strcmp(argv[i], "-") == 0 ? stdin : fopen(argv[i], "r");
+        if (!f) {
+            fprintf(stderr, "wc: cannot open %s\n", argv[i]);
+            continue;
+        }
+        lines = words = chars = 0;
+        int c, in_word = 0;
+        while ((c = fgetc(f)) != EOF) {
+            chars++;
+            if (c == '\n') lines++;
+            if (isspace(c)) in_word = 0;
+            else if (!in_word) { words++; in_word = 1; }
+        }
+        printf("%10d %10d %10d %s\n", lines, words, chars, argv[i]);
+        total_lines += lines;
+        total_words += words;
+        total_chars += chars;
+        if (f != stdin) fclose(f);
+    }
+
+    if (argc > 2)
+        printf("%10d %10d %10d total\n", total_lines, total_words, total_chars);
+
     return 0;
 }
