d9848af66a8553d7f8800caeb5606d3e893dca3b
twbaty <51901395+twbaty@users.noreply.github.com>
2025-10-23 20:06:46 -0500

    Update grep.c

--- src/coreutils/grep.c
+++ src/coreutils/grep.c
@@ -1,83 +1,76 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
+#include <io.h>
+#include <unistd.h>
 
-static int case_insensitive = 0;
-static int show_line_numbers = 0;
-static int invert_match = 0;
+#define COLOR_MATCH "\033[1;31m"  // bright red
+#define COLOR_RESET "\033[0m"
 
-static int match_line(const char *line, const char *pattern) {
-    if (case_insensitive) {
-        for (size_t i = 0; line[i] && pattern[i]; i++) {
-            char a = tolower(line[i]);
-            char b = tolower(pattern[i]);
-            if (a != b) return 0;
+static bool color_enabled = false;
+static bool color_auto = true;
+static bool color_always = false;
+
+static void parse_color_flag(int *argc, char **argv) {
+    for (int i = 1; i < *argc; ++i) {
+        if (strncmp(argv[i], "--color", 7) == 0 || strncmp(argv[i], "--colour", 8) == 0) {
+            const char *arg = strchr(argv[i], '=');
+            if (arg) arg++;
+            else if (i + 1 < *argc) arg = argv[++i];
+            else arg = "auto";
+
+            if (strcmp(arg, "always") == 0) { color_enabled = true; color_auto = false; color_always = true; }
+            else if (strcmp(arg, "never") == 0) { color_enabled = false; color_auto = false; }
+            else { color_auto = true; }
+
+            for (int j = i; j + 1 < *argc; ++j) argv[j] = argv[j + 1];
+            (*argc)--;
+            return;
         }
-        return strstr(strlwr(strdup(line)), strlwr(strdup(pattern))) != NULL;
-    } else {
-        return strstr(line, pattern) != NULL;
+    }
+}
+
+static void highlight_and_print(const char *line, const char *pattern) {
+    const char *p = line;
+    size_t plen = strlen(pattern);
+    while (*p) {
+        const char *match = strcasestr(p, pattern);
+        if (!match) {
+            fputs(p, stdout);
+            break;
+        }
+        fwrite(p, 1, match - p, stdout);
+        if (color_enabled) fputs(COLOR_MATCH, stdout);
+        fwrite(match, 1, plen, stdout);
+        if (color_enabled) fputs(COLOR_RESET, stdout);
+        p = match + plen;
     }
 }
 
 int main(int argc, char *argv[]) {
+    parse_color_flag(&argc, argv);
+    if (color_auto && isatty(STDOUT_FILENO))
+        color_enabled = true;
+
     if (argc < 2) {
-        fprintf(stderr, "Usage: grep [-i] [-n] [-v] <pattern> [file]\n");
+        fprintf(stderr, "Usage: grep [--color=auto|always|never] <pattern> [file]\n");
         return 1;
     }
 
-    int arg_index = 1;
-    while (arg_index < argc && argv[arg_index][0] == '-') {
-        if (strcmp(argv[arg_index], "-i") == 0)
-            case_insensitive = 1;
-        else if (strcmp(argv[arg_index], "-n") == 0)
-            show_line_numbers = 1;
-        else if (strcmp(argv[arg_index], "-v") == 0)
-            invert_match = 1;
-        else {
-            fprintf(stderr, "Unknown option: %s\n", argv[arg_index]);
-            return 1;
-        }
-        arg_index++;
-    }
-
-    if (arg_index >= argc) {
-        fprintf(stderr, "Missing pattern\n");
-        return 1;
-    }
-
-    const char *pattern = argv[arg_index++];
-    FILE *fp = NULL;
-
-    if (arg_index < argc) {
-        fp = fopen(argv[arg_index], "r");
-        if (!fp) {
-            perror("grep");
-            return 1;
-        }
-    } else {
-        fp = stdin;
-    }
+    const char *pattern = argv[1];
+    FILE *fp = (argc > 2) ? fopen(argv[2], "r") : stdin;
+    if (!fp) { perror("grep"); return 1; }
 
     char line[4096];
-    long line_num = 0;
-    int matched_any = 0;
-
     while (fgets(line, sizeof(line), fp)) {
-        line_num++;
-        int matched = match_line(line, pattern);
-        if (invert_match) matched = !matched;
-
-        if (matched) {
-            matched_any = 1;
-            if (show_line_numbers)
-                printf("%ld:", line_num);
-            fputs(line, stdout);
+        if (strcasestr(line, pattern)) {
+            highlight_and_print(line, pattern);
+            if (line[strlen(line) - 1] != '\n') putchar('\n');
         }
     }
 
-    if (fp != stdin)
-        fclose(fp);
-
-    return matched_any ? 0 : 1;
+    if (fp != stdin) fclose(fp);
+    return 0;
 }
