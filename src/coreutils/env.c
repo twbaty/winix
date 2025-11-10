463a91d452037483d12615baad03d1b207db9f27
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-09 22:59:59 -0600

    Update env.c

--- src/coreutils/env.c
+++ src/coreutils/env.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
 #ifdef _WIN32
-    extern char **__p__environ;   // MinGW’s official CRT symbol
-    char **p = __p__environ;
+  #include <windows.h>
+
+  int main(void) {
+      LPCH env = GetEnvironmentStringsA();  // returns double-NUL-terminated block
+      if (!env) return 1;
+
+      for (LPCH p = env; *p; ) {
+          puts(p);
+          while (*p) ++p;   // advance to NUL of this string
+          ++p;              // move to start of next string (or final NUL)
+      }
+
+      FreeEnvironmentStringsA(env);
+      return 0;
+  }
+
 #else
-    extern char **environ;        // POSIX systems
-    char **p = environ;
+  extern char **environ;
+
+  int main(void) {
+      for (char **p = environ; *p; ++p)
+          puts(*p);
+      return 0;
+  }
 #endif
-
-    for (; *p; ++p)
-        puts(*p);
-
-    return 0;
-}
